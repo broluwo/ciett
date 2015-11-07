3a963a8fe31258074159ea79d73f584b55268a46
Brian Oluwo <broluwo@umd.edu>
2015-11-07 11:09:29 -0500

    Tauqir can't commit and it's all my fault.

--- node/spa/js/main.js
+++ node/spa/js/main.js
@@ -1,186 +1,80 @@
-/* Copyright 2013 Chris Wilson
+var ciett = ciett || {};
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-
-var audioContext = new AudioContext();
-var audioInput = null,
-    realAudioInput = null,
-    inputPoint = null,
-    audioRecorder = null;
-var rafID = null;
-var analyserContext = null;
-var canvasWidth, canvasHeight;
-var recIndex = 0;
-
-/* TODO:
-
-- offer mono option
-- "Monitor input" switch
-*/
-
-function saveAudio() {
-    audioRecorder.exportWAV( doneEncoding );
-    // could get mono instead by saying
-    // audioRecorder.exportMonoWAV( doneEncoding );
-}
-
-function gotBuffers( buffers ) {
-    var canvas = document.getElementById( "wavedisplay" );
-
-    drawBuffer( canvas.width, canvas.height, canvas.getContext('2d'), buffers[0] );
-
-    // the ONLY time gotBuffers is called is right after a new recording is completed - 
-    // so here's where we should set up the download.
-    audioRecorder.exportWAV( doneEncoding );
-}
-
-function doneEncoding( blob ) {
-    Recorder.setupDownload( blob, "myRecording" + ((recIndex<10)?"0":"") + recIndex + ".wav" );
-    recIndex++;
-}
-
-function toggleRecording( e ) {
-    if (e.classList.contains("recording")) {
-        // stop recording
-        audioRecorder.stop();
-        e.classList.remove("recording");
-        audioRecorder.getBuffers( gotBuffers );
-    } else {
-        // start recording
-        if (!audioRecorder)
-            return;
-        e.classList.add("recording");
-        audioRecorder.clear();
-        audioRecorder.record();
-    }
-}
-
-function convertToMono( input ) {
-    var splitter = audioContext.createChannelSplitter(2);
-    var merger = audioContext.createChannelMerger(2);
-
-    input.connect( splitter );
-    splitter.connect( merger, 0, 0 );
-    splitter.connect( merger, 0, 1 );
-    return merger;
-}
-
-function cancelAnalyserUpdates() {
-    window.cancelAnimationFrame( rafID );
-    rafID = null;
-}
-
-function updateAnalysers(time) {
-    if (!analyserContext) {
-        var canvas = document.getElementById("analyser");
-        canvasWidth = canvas.width;
-        canvasHeight = canvas.height;
-        analyserContext = canvas.getContext('2d');
-    }
-
-    // analyzer draw code here
-    {
-        var SPACING = 3;
-        var BAR_WIDTH = 1;
-        var numBars = Math.round(canvasWidth / SPACING);
-        var freqByteData = new Uint8Array(analyserNode.frequencyBinCount);
-
-        analyserNode.getByteFrequencyData(freqByteData); 
-
-        analyserContext.clearRect(0, 0, canvasWidth, canvasHeight);
-        analyserContext.fillStyle = '#F6D565';
-        analyserContext.lineCap = 'round';
-        var multiplier = analyserNode.frequencyBinCount / numBars;
-
-        // Draw rectangle for each frequency bin.
-        for (var i = 0; i < numBars; ++i) {
-            var magnitude = 0;
-            var offset = Math.floor( i * multiplier );
-            // gotta sum/average the block, or we miss narrow-bandwidth spikes
-            for (var j = 0; j< multiplier; j++)
-                magnitude += freqByteData[offset + j];
-            magnitude = magnitude / multiplier;
-            var magnitude2 = freqByteData[i * multiplier];
-            analyserContext.fillStyle = "hsl( " + Math.round((i*360)/numBars) + ", 100%, 50%)";
-            analyserContext.fillRect(i * SPACING, canvasHeight, BAR_WIDTH, -magnitude);
-        }
-    }
-    
-    rafID = window.requestAnimationFrame( updateAnalysers );
-}
-
-function toggleMono() {
-    if (audioInput != realAudioInput) {
-        audioInput.disconnect();
-        realAudioInput.disconnect();
-        audioInput = realAudioInput;
-    } else {
-        realAudioInput.disconnect();
-        audioInput = convertToMono( realAudioInput );
-    }
-
-    audioInput.connect(inputPoint);
-}
-
-function gotStream(stream) {
-    inputPoint = audioContext.createGain();
-
-    // Create an AudioNode from the stream.
-    realAudioInput = audioContext.createMediaStreamSource(stream);
-    audioInput = realAudioInput;
-    audioInput.connect(inputPoint);
-
-//    audioInput = convertToMono( input );
-
-    analyserNode = audioContext.createAnalyser();
-    analyserNode.fftSize = 2048;
-    inputPoint.connect( analyserNode );
-
-    audioRecorder = new Recorder( inputPoint );
-
-    zeroGain = audioContext.createGain();
-    zeroGain.gain.value = 0.0;
-    inputPoint.connect( zeroGain );
-    zeroGain.connect( audioContext.destination );
-    updateAnalysers();
-}
-
-function initAudio() {
-        if (!navigator.getUserMedia)
-            navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-        if (!navigator.cancelAnimationFrame)
-            navigator.cancelAnimationFrame = navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
-        if (!navigator.requestAnimationFrame)
-            navigator.requestAnimationFrame = navigator.webkitRequestAnimationFrame || navigator.mozRequestAnimationFrame;
-
-    navigator.getUserMedia(
-        {
-            "audio": {
-                "mandatory": {
-                    "googEchoCancellation": "false",
-                    "googAutoGainControl": "false",
-                    "googNoiseSuppression": "false",
-                    "googHighpassFilter": "false"
-                },
-                "optional": []
-            },
-        }, gotStream, function(e) {
-            alert('Error getting audio');
-            console.log(e);
+var getUserMedia = navigator.mediaDevices ?
+    navigator.mediaDevices.getUserMedia.bind(navigator.mediaDevices) :
+    function (c) {
+        return new Promise(function (f, r) {
+            navigator.webkitGetUserMedia(c, f, r);
         });
-}
+    };
 
-window.addEventListener('load', initAudio );
+ciett.aContext = null;
+ciett.recorder = null;
+navigator.getUserMedia = navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
+
+ciett.hasGetUserMedia_ = function() {
+    ciett.userMedia_ = (navigator.getUserMedia || navigator.webkitGetUserMedia ||
+			navigator.mozGetUserMedia || navigator.msGetUserMedia);
+    return ciett.userMedia_ !== undefined;
+};
+
+/**
+ * When a certain button is clicked on the page launch the audio access flow,
+ * then start recording.
+ */
+ciett.onAudioIconClick = function() {
+    if (ciett.hasGetUserMedia_()) {
+	getUserMedia({audio: true})
+	    .then(function(stream) {
+		ciett.aContext = ciett.aContext || new AudioContext();
+		window.AudioContext = window.AudioContext || window.webkitAudioContext;
+		console.log('Given audio capture access. Providing input to recorder.');
+		ciett.recorder = ciett.recorder || new Recorder(ciett.aContext.createMediaStreamSource(stream));
+		ciett.startRecording();
+	    })
+	    .catch(function(err) {
+		console.error('Was not able to create audio stream.' + err);
+	    });
+    }
+};
+
+/**
+ * Starts the recording.
+ */
+ciett.startRecording = function() {
+    if (undefined !== ciett.recorder) {
+	ciett.recorder.record();
+	console.log('Recording...');
+    }
+};
+
+/**
+ * Stops the recording and then sends it for recording.
+ */
+ciett.stopRecording = function() {
+    if (undefined !== ciett.recorder) {
+	ciett.recorder.stop();
+	console.log('Stopped Recording...');
+	
+	//ciett.recorder.exportWAV(ciett.sendAudio);
+  document.getElementById('elmotothis').textContent = 'Yes, you can Elmo to this!';
+    }
+};
+
+/*
+ * Sends the data using a XHR to the Server for processing and suggestion 
+ * creation.
+ *@param{!Blob} blob - Holds the wav data. 
+ */
+ciett.sendAudio = function(blob){
+    if (undefined !== ciett.recorder) {
+	var xhr = new XMLHttpRequest();
+	var form = new FormData();
+	form.append('audio', blob,'audio.wav');
+	xhr.open('POST','/process',true);
+	xhr.send(form);
+    }
+};
